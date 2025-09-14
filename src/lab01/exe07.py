4b1b06f00514df18c0bce5c904b1f70a349445f5
TemaTik <abatmanov2007@gmail.com>
2025-09-14 21:29:32 +0300

    Update

--- src/lab01/exe07.py
+++ src/lab01/exe07.py
@@ -1,9 +1,13 @@
-ishod = input()
-print("in: " + ishod)
-c = ''
+ishod = input("in: ")
 for i in range(len(ishod)):
     if ishod[i].isupper():
-        cnt = i
+        first_let = i
         break
-c = ''.join([ishod[i] for i in range(cnt, len(ishod), 3)])
+numbers_list = '0123456789'
+for i in range(len(ishod)):
+    if ishod[i] in numbers_list and ishod[i+1] not in numbers_list:
+        seconf_let = i+1;
+        break
+distance = seconf_let - first_let
+c = ''.join([ishod[i] for i in range(first_let, len(ishod), distance)])
 print("out:", c)
